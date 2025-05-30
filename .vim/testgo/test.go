e852abcbe3b62b407efd4cdb841ebd15bf5f0681
kaan <kaandesu00@gmail.com>
2025-05-30 17:37:35 +0200

    test

---
+++ .vim/testgo/test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/charmbracelet/bubbles/timer"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type model struct {
+	views tea.Model
+	timer timer.Model
+}
+
+func main() {
+	fmt.Println()
+
+}
